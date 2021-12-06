224ab17448432de98483f4692ac9767f405f6a16
mariamartinux <mmariaester@gmail.com>
2021-12-06 12:36:27 +0100

    update

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log("up and running");
+
+let express = require("express"); //loading expr library ina variable
+
+let app = express(); //activated
+
+let port = 3000;
+
+let server = app.listen(port); //connection will happen on port 3000
+
+console.log("server is running on http://localhost:" + port);
+
+app.use(express.static("public"));
