75da42f1bc9393574b45864e270d59700f386089
mariamartinux <mmariaester@gmail.com>
2021-12-06 21:29:10 +0100

    update2

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ let server = app.listen(port); //connection will happen on port 3000
 console.log("server is running on http://localhost:" + port);
 
 app.use(express.static("public"));
+
+let serverSocket = require("socket.io");
+
+let io = serverSocket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
